9dd77608c1cd322de79596e24305cd37a9bdc6ed
vtarmo <tarmo.katmuk@gmail.com>
2023-01-26 19:13:44 +0200

    Brew formula update for cephmgr version v0.1.1

--- cephmgr.rb
+++ cephmgr.rb
@@ -5,11 +5,11 @@
 class Cephmgr < Formula
   desc ""
   homepage "https://github.com/vtarmo/cephmgr"
-  version "0.1"
+  version "0.1.1"
 
   on_macos do
-    url "https://github.com/vtarmo/cephmgr/releases/download/v0.1/cephmgr_0.1_darwin_all.tar.gz"
-    sha256 "a86c00fa100d8ad0cec1f4e3627a271d19a168471680086ede1bb17fa9f40bed"
+    url "https://github.com/vtarmo/cephmgr/releases/download/v0.1.1/cephmgr_0.1.1_darwin_all.tar.gz"
+    sha256 "702abae09fde88e8cdce7081c971909704d3b69424748eebea9e7d3f4c37d97f"
 
     def install
       bin.install "cephmgr"
@@ -18,16 +18,16 @@ class Cephmgr < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/vtarmo/cephmgr/releases/download/v0.1/cephmgr_0.1_linux_arm64.tar.gz"
-      sha256 "42b0bf481bf2d0c1c538411e4d04d191c439b94154e46205a80db642b80ddd1e"
+      url "https://github.com/vtarmo/cephmgr/releases/download/v0.1.1/cephmgr_0.1.1_linux_arm64.tar.gz"
+      sha256 "4c5a7cc7df545758fb0bc9bb7367d1083542767ed3dfc234607f0646787b926c"
 
       def install
         bin.install "cephmgr"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/vtarmo/cephmgr/releases/download/v0.1/cephmgr_0.1_linux_amd64.tar.gz"
-      sha256 "54650795fb44b790be71fbdc60e552a72fd313209e87bc5d81f6a11e24512e85"
+      url "https://github.com/vtarmo/cephmgr/releases/download/v0.1.1/cephmgr_0.1.1_linux_amd64.tar.gz"
+      sha256 "80c79b6cf73d0bf3181f24eaa90c4bd8cd9cc140379c2ea95a13bf36f2a98b60"
 
       def install
         bin.install "cephmgr"
